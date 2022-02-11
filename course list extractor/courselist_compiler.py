65f2145ade13099111c3f140862266a05e0bb2a6
Patipol-BKK <53873809+Patipol-BKK@users.noreply.github.com>
2022-02-11 14:33:05 +0800

    Add files via upload

--- course list extractor/courselist_compiler.py
+++ course list extractor/courselist_compiler.py
@@ -70,5 +70,83 @@ for filename in os.listdir(path):
 		print(file_count)
 		file_count += 1
 
+# string_list = ['']
+# bracket_depth = 0
+# prev1_bracket_depth = 0
+# prev2_bracket_depth = 0
+# for filename in os.listdir(path):
+# 	with open(os.path.join(path, filename), 'r') as f:
+# 		for line in f:
+# 			for char in line:
+# 				if char == '<':
+# 					bracket_depth += 1
+# 				elif char == '>':
+# 					bracket_depth -= 1
+# 				else:
+
+# 					if not bracket_depth:
+# 						if prev2_bracket_depth == 1:
+# 							string_list.append('')
+# 						string_list[len(string_list) - 1] = string_list[len(string_list) - 1] + char
+# 				prev2_bracket_depth = prev1_bracket_depth
+# 				prev1_bracket_depth = bracket_depth
+
+# def filter_enter(char):
+# 	return not char == '\n' and not char == '\n\n' and not char == ''
+
+# for i, string in enumerate(string_list):
+# 	if string == '\n' :
+# 		string_list.pop(i)
+# # string_list = list(filter(filter_enter, string_list))
+
+# columns = [ 'course_code',
+# 			'course_name',
+# 			'AU',
+# 			'prerequisite',
+# 			'mutually_exclusive_with',
+# 			'not_avail_to_program',
+# 			'not_avail_to_program_with',
+# 			'description']
+# new_row = dict()
+
+# state = 0
+# title_state = 0
+# for string in string_list:
+# 	if bool(re.match("[A-Z][A-Z][0-9][0-9][0-9][0-9]", string)) and len(string) == 6:
+# 		print(new_row)
+# 		state = 0
+# 		new_row['course_code'] = string
+# 		title_state = 1
+# 		continue
+# 	if string == 'Prerequisite:':
+# 		state = 1
+# 		continue
+# 	if string == 'Grade Type:':
+# 		state = 2
+# 		continue
+# 	if string == 'Mutually exclusive with:':
+# 		state = 3
+# 		continue
+# 	if string == 'Not available to Programme:':
+# 		state = 4
+# 		continue
+# 	if string == 'Not available to all Programme with:':
+# 		state = 5
+# 		continue
+
+# 	if state == 0:
+# 		if title_state == 1:
+# 			new_row['course_name'] = string
+# 		elif title_state == 2:
+# 			new_row['AU'] = string
+# 		title_state += 1
+# 	elif state == 1:
+# 		try:
+# 			new_row['prerequisite'] += string
+# 		except:
+# 			new_row['prerequisite'] = string
+
+# print('\n\n\n\n\n\n')
+# print(string_list)
 courses = pd.DataFrame(courses_list)
 courses.to_excel('courses.xlsx')
